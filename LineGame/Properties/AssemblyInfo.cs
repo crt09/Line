0c94df4291d8aa2cfbd112d9edd561baabfe29c4
tougit <dr.light2215@gmail.com>
2019-02-02 18:33:05 +0300

    Setup assembly

--- LineGame/Properties/AssemblyInfo.cs
+++ LineGame/Properties/AssemblyInfo.cs
@@ -1,12 +1,11 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("LineGame")]
-[assembly: AssemblyProduct("LineGame")]
+[assembly: AssemblyTitle("Line")]
+[assembly: AssemblyProduct("Line")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyCompany("")]
